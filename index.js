9270eed0b794db637a34de8d4b989c1db3c4e433
priyanshu <yadavpriyanshu1y@gmail.com>
2024-06-02 11:14:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+import express from 'express';
+
+
+const app = express();
+const port = process.env.PORT | 3000;
+app.listen(port);
+
+app.use(express.json());
+
+const user = {
+    username: "falana",
+    password: "dikana"
+}
+
+app.get('/login', (req, res)=>{
+    const { username, password } = req.body;
+    console.log(username, password);
+    res.send("this is working");
+});
